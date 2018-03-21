8d0b90d18ecdbe8d6f79c5a9c4320985813ca4c7
Anne Mennen <amennen@princeton.edu>
2018-03-21 17:42:29 -0400

    plotting for new values inflection pt

--- plottingtransferfn.m
+++ plottingtransferfn.m
@@ -1,17 +1,17 @@
 
-gain = 4;
-x_shift = .12;
-y_shift = .25;
-steepness = .6;
-cs = -1:.1:1;
-y2 = steepness./(1+exp(-gain*(cs-x_shift)))+y_shift;
-
 gain = 3;
 x_shift = .2;
-y_shift = .15;
+y_shift = 0.15;
 steepness = .9;
 cs = -1:.1:1;
 y = steepness./(1+exp(-gain*(cs-x_shift)))+y_shift;
+
+gain = 2.75;
+x_shift = .2;
+y_shift = 0.05;
+steepness = .9;
+cs = -1:.1:1;
+y2 = steepness./(1+exp(-gain*(cs-x_shift)))+y_shift;
 
 figure;
 plot(cs,y)
@@ -19,7 +19,7 @@ hold on;
 plot(cs,y2, 'r');
 legend('original', 'proposed')
 xlabel('Category separation');
-ylabel('Opacity proportion');
-title('Transfer functions')
+ylabel('Attended category opacity proportion');
+title('Transfer function')
 ylim([0 1])
 set(findall(gcf,'-property','FontSize'),'FontSize',20)
