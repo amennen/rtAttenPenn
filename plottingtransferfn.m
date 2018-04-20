32892db251be701f492c15a371ea64cdda1b169f
Anne Mennen <amennen@princeton.edu>
2018-04-19 22:06:05 -0400

    plotting transfer fn change

--- plottingtransferfn.m
+++ plottingtransferfn.m
@@ -1,10 +1,10 @@
 
-gain = 3;
-x_shift = .2;
-y_shift = 0.15;
-steepness = .9;
-cs = -1:.1:1;
-y = steepness./(1+exp(-gain*(cs-x_shift)))+y_shift;
+% gain = 3;
+% x_shift = .2;
+% y_shift = 0.15;
+% steepness = .9;
+% cs = -1:.1:1;
+% y = steepness./(1+exp(-gain*(cs-x_shift)))+y_shift;
 
 gain = 2.75;
 x_shift = .2;
@@ -14,8 +14,8 @@ cs = -1:.1:1;
 y2 = steepness./(1+exp(-gain*(cs-x_shift)))+y_shift;
 
 figure;
-plot(cs,y)
-hold on;
+%plot(cs,y)
+%hold on;
 plot(cs,y2, 'r');
 legend('original', 'proposed')
 xlabel('Category separation');
